f99fcb053b0b37d92b8ae7f6c61eb0ce4ca71120
Patriots <proverbs990@gmail.com>
2020-03-21 17:34:32 -0700

    latest

--- auto.py
+++ auto.py
@@ -1,5 +1,5 @@
-name = input("What is your name?")
-age = input("How old are you?")
-resp = input("Is the auto script working?")
+name = input("What is your name? ")
+age = input("How old are you? ")
+resp = input("Is the auto script working? ")
 
 print(f"Hi {name}. \nYou are {age} years old, \nYou said {resp} to the script working")
