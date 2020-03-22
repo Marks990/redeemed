4a338fbd9552627a2f13a9012c31903e91541680
Super <Proverbs990@gmail.com>
2020-03-21 17:03:09 -0700

    Removed comment

--- auto.py
+++ auto.py
@@ -3,4 +3,3 @@ age = input("How old are you?")
 resp = input("Is the auto script working?")
 
 print(f"Hi {name}. \nYou are {age} years old, \nYou said {resp} to the script working")
-# extra comment
